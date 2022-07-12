a07c14c155aab36e3fa2b546130c480cf4f9d8ff
Eric Williams <egw.egwilliams@gmail.com>
2022-07-12 18:46:52 -0400

    added selection buttons

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// grid creation
 const grid = document.getElementsByClassName("grid");
 
+
+// funcction to create grid based on size
 function createGrid(size=16) {
     // for loops to go across and place did
     for (let i =0; i < size; i++) {
@@ -15,3 +18,7 @@ let cell = document.createElement("div");
 cell = document.createAttribute("class");
 cell.value = "cell";
 grid.appendChild(cell);
+
+
+// slider functionality
+const slider = document.getElementById("myRange");
