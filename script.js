92ae30853743cbf3b22319888aa0bc9360c7ba4f
Eric Williams <egw.egwilliams@gmail.com>
2022-07-14 18:24:05 -0400

    identified necessary constants in script.js

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-// grid creation
+// document variables
 const grid = document.getElementsByClassName("grid");
-
-
-// funcction to create grid based on size
-function createGrid(size=16) {
-    // for loops to go across and place did
-    for (let i =0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-
-        }
-    }
-
-}
-
-let cell = document.createElement("div");
-// give it a class for styling
-cell = document.createAttribute("class");
-cell.value = "cell";
-grid.appendChild(cell);
-
-
-// slider functionality
+const colorPicker = document.getElementById("colorPicker");
+const colorMode = document.getElementById("colorMode");
+const rainbowMode = docuement.getElementById("rainbowMode");
+const eraserBtn = document.getElementById("eraserBtn");
+const clearBtn = document.getElementById("clearBtn");
 const slider = document.getElementById("myRange");
+
+
