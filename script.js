9d79127f58e45909a43d96af2a302095cda5bf94
Eric Williams <egw.egwilliams@gmail.com>
2022-07-18 20:19:00 -0400

    added event listeners

--- script.js
+++ script.js
@@ -16,13 +16,32 @@ let currColor = defaultColor;
 let currMode = startMode;
 let currSize = gridSize;
 
-// function to set new color
-function setColor(newColor) {
+// function to update currColor
+function changeColor(newColor) {
     currColor = newColor;
 }
 
-// function to change mode
+// function to update currMode
 function changeMode(mode) {
     activateButton(mode);
     currMode = mode;
 }
+
+// function to update currSize
+function changeSize(size) {
+    currSize = size;
+}
+/* EVENT LISTENERS */
+// color picker event listener
+colorPicker.oninput = (x) => changeColor(x.target.value);
+// color mode listener
+colorMode.onclick = () => changeMode("color");
+// random mode listener
+rainbowMode.onclick = () => changeMode("rainbow");
+// eraser mode listener
+eraserBtn.onclick = () => changeMode("eraser");
+// clear all mode listener
+clearBtn.onclick = () => refreshGrid();
+// slider listener
+slider.onmousemove = (x) => updateGridDisplay(x.target.value);
+slider.onchange = (x) => updateSize(x.target.value);
