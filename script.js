b0170cf4a4c4234136d0ee2eb4a93db7cfdc2d57
Eric Williams <egw.egwilliams@gmail.com>
2022-07-18 20:41:32 -0400

    added grid creation function, grid fill function

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ function changeColor(newColor) {
 
 // function to update currMode
 function changeMode(mode) {
-    activateButton(mode);
+    // add active class styling to mode
+    activeClass(mode);
     currMode = mode;
 }
 
@@ -45,3 +46,57 @@ clearBtn.onclick = () => refreshGrid();
 // slider listener
 slider.onmousemove = (x) => updateGridDisplay(x.target.value);
 slider.onchange = (x) => updateSize(x.target.value);
+
+// updateSize function
+function updateSize(v) {
+    setSize(v);
+    updateGridDislay(v);
+    refreshGrid();
+}
+
+// updateGridDisplay function (updates section that shows grid size)
+function updateGridDisplay(v) {
+    sliderDisplay.innerHTML = `${v} x ${v}`;
+}
+
+// refresh grid aka clear it
+function refreshGrid() {
+    clearGrid();
+    createGrid(currSize);
+}
+
+// clear grid function, leave it blank
+function clearGrid() {
+    grid.innerHTML = "";
+}
+
+// function to create the grid
+function createGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    // for loop
+    for (let i =0; i < size * size; i++) {
+        const cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.addEventListener("mouseover", fillCell);
+        grid.appendChild(cell);
+    }
+}
+
+// function to fill cells
+function fillCell(x) {
+    // rainbow mode functionality
+    if (currMode == "rainbow") {
+        // get random RGB values
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        // set random color as fill
+        x.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    } else if (currMode == "color" ) {
+        x.target.style.backgroundColor = currColor;
+    } else if (currMode == "eraser") {
+        x.target.style.backgroundColor = "#FFFFFF"
+    }
+}
+
